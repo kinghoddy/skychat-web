7b03b3bdfa096badbaac308c51ce57b76a71c648
kinghoddy <odunmilade@gmail.com>
2020-02-04 12:51:10 +0100

    Efef

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,8 @@ var config = {
     apiKey: 'AIzaSyCKMoKc1Cft0WG1etZLvnmh5ytzdckkIcg',
     databaseURL: "https://skymail-920ab.firebaseio.com",
     storageBucket: "skymail-920ab.appspot.com",
-    authDomain: "skymail-920ab.firebaseapp.com"
+    authDomain: "skymail-920ab.firebaseapp.com",
+
 };
 firebase.initializeApp(config);
 
