92e30016a54eed6ec6d0a3afcc770554d9841833
kinghoddy git config --global user.email odunmilade@gmail.com <you@example.com>
2020-02-04 12:51:51 +0100

    fgfg

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,24 @@
 import * as firebase from "firebase/app";
 
-var config = {
-    apiKey: 'AIzaSyCKMoKc1Cft0WG1etZLvnmh5ytzdckkIcg',
-    databaseURL: "https://skymail-920ab.firebaseio.com",
-    storageBucket: "skymail-920ab.appspot.com",
-    authDomain: "skymail-920ab.firebaseapp.com",
 
-};
 firebase.initializeApp(config);
 
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCKMoKc1Cft0WG1etZLvnmh5ytzdckkIcg",
+    authDomain: "skymail-920ab.firebaseapp.com",
+    databaseURL: "https://skymail-920ab.firebaseio.com",
+    projectId: "skymail-920ab",
+    storageBucket: "skymail-920ab.appspot.com",
+    messagingSenderId: "795125035544",
+    appId: "1:795125035544:web:bc6cddb5e006c3bfdd496d",
+    measurementId: "G-Z3K3MYS9NC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-export default firebase
+
+
+export default firebase;
