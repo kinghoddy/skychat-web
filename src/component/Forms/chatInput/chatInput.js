9ef8cf855749a8d5c09efe62cdec005a0076ce7d
kinghoddy git config --global user.email odunmilade@gmail.com <you@example.com>
2020-03-31 19:22:25 +0100

    dfdf

--- src/component/Forms/chatInput/chatInput.js
+++ src/component/Forms/chatInput/chatInput.js
@@ -17,11 +17,32 @@ const chatInput = props => {
 
   return (
     <form onSubmit={props.sendChat} className={classes.msgInput + " row no-gutters"}>
-      <div className="col-md-11 col-10">
+
+      <div className="col-md-2 col-5 justify-content-around pb-1  d-flex align-items-center" style={{ alignSelf: "flex-end" }}>
+        <button onClick={() => {
+          textInput.style.height = 'auto'
+          props.changeStyle(false)
+        }} className={classes.btn}>
+          <i className="material-icons o" >photo</i>
+        </button>
+        <button onClick={() => {
+          textInput.style.height = 'auto'
+          props.changeStyle(false)
+        }} className={classes.btn}>
+          <i className="material-icons " >videocam</i>
+        </button>
+        <button onClick={() => {
+          textInput.style.height = 'auto'
+          props.changeStyle(false)
+        }} className={classes.btn}>
+          <i className="material-icons " >audiotrack</i>
+        </button>
+      </div>
+      <div className="col pt-1">
         <textarea id="t"
           style={{
             height: height + 'px'
-          }} rows="1" type="text" required onChange={props.changed} value={props.value} placeholder="Type your message" />
+          }} rows="1" type="text" required onChange={props.changed} value={props.value} placeholder="Aa" />
       </div>
       <div className="col-md-1 col-2 pb-1  d-flex align-items-center" style={{ alignSelf: "flex-end" }}>
         <button onClick={() => {
